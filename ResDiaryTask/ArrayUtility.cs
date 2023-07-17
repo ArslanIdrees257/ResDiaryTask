76ff5c34d23dc7852dc7b4ff2375ae778d4b990f
Muhammad Arslan Idrees <88210541+ArslanIdrees257@users.noreply.github.com>
2023-07-17 02:27:40 +0100

    Push Code
    Pushing code into repo

---
+++ ResDiaryTask/ArrayUtility.cs
@@ -0,0 +1,42 @@
+﻿namespace ResDiaryTask;
+
+public class ArrayUtility
+{
+    /*Requirement 
+     Example pseudo-code:
+        groupArrayElements([1, 2, 3, 4, 5], 3);
+        output: [ [ 1, 2 ], [ 3, 4 ], [ 5 ] ]
+    */
+    public T[][] groupArrayElements<T>(T[] inputArray, int outputShape)
+    {
+        #region Requirement 1 statement about output shape
+        /*
+         * Given an array of length >= 0, and a positive integer N, 
+           return the contents of the array divided into N equally sized arrays
+         * Where the size of the original array cannot be divided equally by N, 
+           the final part should have a length equal to the remainder.
+        */
+        #endregion
+        var size = (inputArray.Length % outputShape == 0) ? outputShape : inputArray.Length % outputShape;
+        List<T[]> groupArrays = new List<T[]>();
+        var condition = inputArray.Length % size == 0 ? inputArray.Length / size : inputArray.Length / size + 1;
+        for (int i = 0; i < condition; i++)
+        {
+            var temparr = (i == condition - 1) ? new T[inputArray.Length - (i * size)] : new T[size];
+            #region Uncomment this for manual implementation
+
+            //for (int j = 0; j < temparr.Length; j++)
+            //{
+            //    temparr[j] = inputArray[i * size + j];
+            //}
+            //groupArrays.Add(temparr);
+            #endregion
+            #region using skip and take
+            groupArrays.Add(inputArray.Skip(i * size).Take(size).ToArray());
+            #endregion
+        }
+        return groupArrays.ToArray();
+    }
+}
+
+
---
+++ ResDiaryTaskTest/ArrayUtilityTest.cs
@@ -0,0 +1,71 @@
+﻿using ResDiaryTask;
+namespace ResDiaryTaskTest;
+
+[TestFixture]
+public class ArrayUtilityTest
+{
+
+    private ArrayUtility groupArray;
+    private static IEnumerable<TestCaseData> intdata
+    {
+        get
+        {
+            yield return new TestCaseData(new int[] { 1, 2, 3, 4, 5 }, 3, new int[][]
+            {
+                new int[]{1,2},
+                new int[]{3,4},
+                new int[]{5}
+            });
+            yield return new TestCaseData(new int[] { 1, 2, 3, 4, 5, 6 }, 2, new int[][]
+            {
+                new int[]{1,2},
+                new int[]{3,4},
+                new int[]{5,6}
+            });
+        }
+    }
+    private static IEnumerable<TestCaseData> stringData
+    {
+        get
+        {
+            yield return new TestCaseData(new string[] { "Arslan", "Scott", "Owain", "Ben", "Einstein" }, 3, new string[][]
+            {
+                new string[]{"Arslan", "Scott"},
+                new string[]{"Owain","Ben"},
+                new string[]{ "Einstein" }
+            });
+            yield return new TestCaseData(new string[] { "Arslan", "Scott", "Owain", "Ben", "Einstein", "Newton" }, 2, new string[][]
+            {
+                new string[]{"Arslan", "Scott"},
+                new string[]{"Owain","Ben"},
+                new string[]{"Einstein", "Newton" }
+            });
+            yield return new TestCaseData(new string[] { "Arslan", "Scott", "Owain", "Ben", "Einstein", "Newton" }, 3, new string[][]
+            {
+                new string[]{"Arslan", "Scott","Owain" },
+                new string[]{ "Ben", "Einstein", "Newton" }
+            });
+        }
+    }
+
+
+    [SetUp]
+    public void Setup()
+    {
+        groupArray = new ArrayUtility();
+
+    }
+    [Test]
+    [TestCaseSource(nameof(intdata))]
+    public void intDataTest(int[] input, int shape, int[][] output)
+    {
+        Assert.That(groupArray.groupArrayElements(input, shape), Is.EqualTo(output));
+    }
+    [Test]
+    [TestCaseSource(nameof(stringData))]
+    public void stringDataTest(string[] input, int shape, string[][] output)
+    {
+        Assert.That(groupArray.groupArrayElements(input, shape), Is.EqualTo(output));
+    }
+
+}
